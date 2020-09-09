93ef6ae4745c82d7102412934b7483eaa13b702b
Tr3v1n4t0r <813@holbertonschool.com>
2020-09-09 11:47:35 -0700

    typo fix

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" places_reviews module """
+""" places_amenities module """
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from models import storage
