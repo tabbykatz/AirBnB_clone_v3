d299119ea3793fa88ba55dd6f47e8b618150ff44
tabbykatz <tomelay@gmail.com>
2020-09-10 02:42:30 +0000

    add import User, Place but right this time

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -3,7 +3,9 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from models import storage
-from models.review import Review, User, Place
+from models.review import Review
+from models.user import User
+from models.place import Place
 
 
 @app_views.route('/places/<place_id>/reviews', strict_slashes=False)
